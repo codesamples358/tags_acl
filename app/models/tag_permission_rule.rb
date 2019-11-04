78f80476f3ed67d085606e3bfc8333db85542452
Shurik <shurikp@gmail.com>
2019-11-04 13:10:57 +0300

    cleanup

--- app/models/tag_permission_rule.rb
+++ app/models/tag_permission_rule.rb
@@ -54,50 +54,6 @@ class TagPermissionRule < ActiveRecord::Base
     all.any? {|rule| rule.matches?(user, issue) }
   end
 
-  def self.visible_issues_sql(user, options = {})
-
-    # .joins("LEFT JOIN taggings ON taggings.taggable_id = issues.id AND taggings.context = 'tags' AND taggings.taggable_type = 'Issue'")
-    # .joins('LEFT JOIN tags ON tags.id = taggings.tag_id')
-
-    issues_table = Issue.table_name
-    all_rules    = self.all
-
-    Project.allowed_to_condition(user, :view_issues, options) do |role, user|
-
-      # sql = if user.id && user.logged?
-      #   case role.issues_visibility
-      #   when 'all'
-      #     '1=1'
-      #   when 'default'
-      #     user_ids = [user.id] + user.groups.pluck(:id).compact
-      #     "(#{issues_table}.is_private = #{connection.quoted_false} OR #{issues_table}.author_id = #{user.id} OR #{issues_table}.assigned_to_id IN (#{user_ids.join(',')}))"
-      #   when 'own'
-      #     user_ids = [user.id] + user.groups.pluck(:id).compact
-      #     "(#{issues_table}.author_id = #{user.id} OR #{issues_table}.assigned_to_id IN (#{user_ids.join(',')}))"
-      #   else
-      #     '1=0'
-      #   end
-      # else
-      #   "(#{issues_table}.is_private = #{connection.quoted_false})"
-      # end
-      # unless role.permissions_all_trackers?(:view_issues)
-      #   tracker_ids = role.permissions_tracker_ids(:view_issues)
-      #   if tracker_ids.any?
-      #     sql = "(#{sql} AND #{issues_table}.tracker_id IN (#{tracker_ids.join(',')}))"
-      #   else
-      #     sql = '1=0'
-      #   end
-      # end
-
-      all_rules.map do |rule|
-        if rule.matches_role?(role)
-          op = !rule.tag_not ? '<>' : '='
-          "(tags.name #{op} '#{rule.tag}')"
-        end
-      end.compact.join(' AND ')
-    end
-  end
-
   def update_prohibited
     self.prohibited_issues.delete_all
     update_scope scope
