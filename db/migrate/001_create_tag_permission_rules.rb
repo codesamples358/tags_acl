dcca29f828b8e0e5c3f8a2cd44bf4680625b9398
Shurik <shurikp@gmail.com>
2019-11-04 13:09:06 +0300

    migration fix

--- db/migrate/001_create_tag_permission_rules.rb
+++ db/migrate/001_create_tag_permission_rules.rb
@@ -2,12 +2,14 @@ class CreateTagPermissionRules < ActiveRecord::Migration[5.2]
   def change
     create_table :tag_permission_rules do |t|
       t.string :name
-      t.references :role, foreign_key: true
+      t.integer :role_id
 
       t.string :tag
 
       t.boolean :role_not
       t.boolean :tag_not
     end
+
+    add_index :tag_permission_rules, :role_id
   end
 end
